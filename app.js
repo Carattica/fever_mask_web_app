5d46211ab3b1a073b9c68023193370c01df3d0b6
20mattk <Henley8669!>
2022-02-01 21:14:47 -0500

    Added in various style sheets for the views I will implement.

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var violationsHomeRouter = require('./routes/violations_home');
+var registerRouter = require('./routes/register');
+var violationsWeeklyRouter = require('./routes/violations_weekly');
 
 var app = express();
 
@@ -22,9 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/violations_home', violationsHomeRouter);
+app.use('/register', registerRouter);
+app.use('/violations_weekly', violationsWeeklyRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('Here is the users page.');
+    res.send('Register Page');
 });
 
 module.exports = router;
