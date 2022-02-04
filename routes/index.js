61333496305de0bc73193b992662d857d7c55e1d
20mattk <Henley8669!>
2022-02-03 21:45:21 -0500

    Added functionality to redirect from index (home) to the login page as the default landing page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
-});
+// redirect to login page
+router.get('/', (req, res) => {
+  res.redirect('/login');
+})
 
 module.exports = router;
