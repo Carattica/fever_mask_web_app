39b57df226f8b6975c056f3cd65d259244003c29
20mattk <Henley8669!>
2022-02-03 21:47:32 -0500

    Minor code comments.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// getting the router files
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var violationsHomeRouter = require('./routes/violations_home');
@@ -17,12 +18,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// integrating the router files and sending paths
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/violations_home', violationsHomeRouter);
