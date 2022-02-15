38f5a682abfdb3ae872ec8e7488f46b2fb69cd0d
20mattk <Henley8669!>
2022-02-14 22:35:08 -0500

    Dynamically displaying found vio docs (unstlyized)

--- routes/violations.js
+++ routes/violations.js
@@ -12,10 +12,17 @@ router.get('/register', forwardAuthenticated, (req, res) => res.render('register
 router.get('/violations_weekly', ensureAuthenticated, (req, res) => res.render('violations_weekly'));
 router.get('/control_access', ensureAuthenticated, (req, res) => res.render('control_access'));
 
+// time, date, location, type of offense
 router.get('/violations_home', ensureAuthenticated, (req, res) => {
-    Violation.findOne({violationType: 'mask'}).then(violation => {
+    Violation.find({}).then(violation => {
         if (violation) {
-            res.render('violations_home', {vio: violation['violationType'], img: violation['imageUrl']});
+            // get this loop to EJS side to display in HTML list
+            console.log('VIOLATIONS FOUND:');
+            for (let i = 0; i < violation.length; i++) {
+                console.log(violation[i]['violationType'], violation[i]['imageUrl']);
+            }
+            // res.render('violations_home', {vio: violation[1]['violationType'], img: violation[1]['imageUrl']});
+            res.render('violations_home', {vios: violation});
         }
         else {
             console.log('Error fetching violations from Violations Database.');
